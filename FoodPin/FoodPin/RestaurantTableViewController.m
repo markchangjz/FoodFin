921f3c95b82cc4d32961655f4ed11364a3a23484
MarkChang <shea4a71@gmail.com>
2017-03-17 22:09:34 +0800

    Add restaurant location and type data.

--- FoodPin/FoodPin/RestaurantTableViewController.m
+++ FoodPin/FoodPin/RestaurantTableViewController.m
@@ -12,6 +12,8 @@
 @interface RestaurantTableViewController () {
 	NSArray *restaurantNames;
 	NSArray *restaurantImages;
+	NSArray *restaurantLocations;
+	NSArray *restaurantTypes;
 }
 
 @end
@@ -24,6 +26,10 @@
 	restaurantNames = @[@"Cafe Deadend", @"Homei", @"Teakha", @"Cafe Loisl", @"Petite Oyster", @"For Kee Restaurant", @"Po's Atelier", @"Bourke Street Bakery", @"Haigh's Chocolate", @"Palomino Espresso", @"Upstate", @"Traif", @"Graham Avenue Meats", @"Waffle & Wolf", @"Five Leaves", @"Cafe Lore", @"Confessional", @"Barrafina", @"Donostia", @"Royal Oak", @"Thai Cafe"];
 
 	restaurantImages = @[@"cafedeadend.jpg", @"homei.jpg", @"teakha.jpg", @"cafeloisl.jpg", @"petiteoyster.jpg", @"forkeerestaurant.jpg", @"posatelier.jpg", @"bourkestreetbakery.jpg", @"haighschocolate.jpg", @"palominoespresso.jpg", @"upstate.jpg", @"traif.jpg", @"grahamavenuemeats.jpg", @"wafflewolf.jpg", @"fiveleaves.jpg", @"cafelore.jpg", @"confessional.jpg", @"barrafina.jpg", @"donostia.jpg", @"royaloak.jpg", @"thaicafe.jpg"];
+
+	restaurantLocations = @[@"Hong Kong", @"Hong Kong", @"Hong Kong", @"Hong Kong", @"Hong Kong", @"Hong Kong", @"Hong Kong", @"Sydney", @"Sydney", @"Sydney", @"New York", @"New York", @"New York", @"New York", @"New York", @"New York", @"New York", @"London", @"London", @"London", @"London"];
+
+	restaurantTypes = @[@"Coffee & Tea Shop", @"Cafe", @"Tea House", @"Austrian / Causual Drink", @"French", @"Bakery", @"Bakery", @"Chocolate", @"Cafe", @"American / Seafood", @"American", @"American", @"Breakfast & Brunch", @"Coffee & Tea", @"Coffee & Tea", @"Latin American", @"Spanish", @"Spanish", @"Spanish", @"British", @"Thai"];
 }
 
 - (void)viewDidLoad {
@@ -56,6 +62,8 @@
 	RestaurantTableViewCell *cell = (RestaurantTableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     
 	cell.nameLabel.text = restaurantNames[indexPath.row];
+	cell.locationLabel.text = restaurantLocations[indexPath.row];
+	cell.typeLabel.text = restaurantTypes[indexPath.row];
 	cell.thumbnailImageView.image = [UIImage imageNamed:restaurantImages[indexPath.row]];
 
     return cell;
