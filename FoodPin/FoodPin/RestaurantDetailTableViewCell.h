4a52a31029f795069d1cfa4030d34eccb284a630
MarkChang <shea4a71@gmail.com>
2017-03-18 21:50:22 +0800

    Add RestaurantDetailTableViewCell.

---
+++ FoodPin/FoodPin/RestaurantDetailTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantDetailTableViewCell.h
+//  FoodPin
+//
+//  Created by MarkChang on 2017/3/18.
+//  Copyright © 2017年 MarkChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantDetailTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *fieldLabel;
+@property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+
+@end
---
+++ FoodPin/FoodPin/RestaurantDetailTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailTableViewCell.m
+//  FoodPin
+//
+//  Created by MarkChang on 2017/3/18.
+//  Copyright © 2017年 MarkChang. All rights reserved.
+//
+
+#import "RestaurantDetailTableViewCell.h"
+
+@implementation RestaurantDetailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
