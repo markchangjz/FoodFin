e31f92963e73a0b6fac70407cb104c3718e6fe20
MarkChang <shea4a71@gmail.com>
2017-04-06 12:32:33 +0800

    Add walkthrough content view.

---
+++ FoodPin/FoodPin/WalkthroughContentViewController.h
@@ -0,0 +1,22 @@
+//
+//  WalkthroughContentViewController.h
+//  FoodPin
+//
+//  Created by MarkChang on 2017/4/6.
+//  Copyright © 2017年 MarkChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WalkthroughContentViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *headingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *contentImageView;
+
+@property (nonatomic) NSInteger *index;
+@property (copy, nonatomic) NSString *heading;
+@property (copy, nonatomic) NSString *content;
+@property (copy, nonatomic) NSString *imageFile;
+
+@end
---
+++ FoodPin/FoodPin/WalkthroughContentViewController.m
@@ -0,0 +1,41 @@
+//
+//  WalkthroughContentViewController.m
+//  FoodPin
+//
+//  Created by MarkChang on 2017/4/6.
+//  Copyright © 2017年 MarkChang. All rights reserved.
+//
+
+#import "WalkthroughContentViewController.h"
+
+@interface WalkthroughContentViewController ()
+
+@end
+
+@implementation WalkthroughContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+	self.headingLabel.text = self.heading;
+	self.contentLabel.text = self.content;
+	self.contentImageView.image = [UIImage imageNamed:self.imageFile];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
